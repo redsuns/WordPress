68ab8163181d0c5c3345e6480b7a7e8361927dea
Andre Cardoso <andrecardosodev@gmail.com>
2015-02-10 13:41:52 -0200

    Setado pt_BR como padrão

--- wp-config-global.php
+++ wp-config-global.php
@@ -70,6 +70,8 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define('WP_LANG', 'pt_BR');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
